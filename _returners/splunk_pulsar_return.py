4862b6cbc2daa682b9794cbcc13d9915eeb5b068
Colton Myers <colton.myers@gmail.com>
2016-10-12 16:41:48 -0600

    Add support for win_pulsar to splunk_pulsar_return

--- _returners/splunk_pulsar_return.py
+++ _returners/splunk_pulsar_return.py
@@ -27,6 +27,7 @@ import socket
 # Imports for http event forwarder
 import requests
 import json
+import os
 import time
 from collections import defaultdict
 
@@ -68,54 +69,93 @@ def returner(ret):
 
     for item in data:
         alert = item['return']
-        # The second half of the change will be "|IN_ISDIR" for directories
-        change = alert['change'].split("|")[0]
-        # Skip the IN_IGNORED events
-        if change == "IN_IGNORED":
-            continue
-        if len(alert['change'].split("|")) == 2:
-            object_type = 'directory'
-        else:
-            object_type = 'file'
-
-        actions = defaultdict(lambda: 'unknown')
-        actions['IN_ACCESS'] = 'read'
-        actions['IN_ATTRIB'] = 'acl_modified'
-        actions['IN_CLOSE_NOWRITE'] = 'read'
-        actions['IN_CLOSE_WRITE'] = 'read'
-        actions['IN_CREATE'] = 'created'
-        actions['IN_DELETE'] = 'deleted'
-        actions['IN_DELETE_SELF'] = 'deleted'
-        actions['IN_MODIFY'] = 'modified'
-        actions['IN_MOVE_SELF'] = 'modified'
-        actions['IN_MOVED_FROM'] = 'modified'
-        actions['IN_MOVED_TO'] = 'modified'
-        actions['IN_OPEN'] = 'read'
-        actions['IN_MOVE'] = 'modified'
-        actions['IN_CLOSE'] = 'read'
-        expected_cim_values = {'action': actions}
-
         event = {}
         payload = {}
-        event["action"] = expected_cim_values['action'][change]
-        event["change_type"] = 'filesystem'
-        event["object_category"] = object_type
-        event["object_path"] = alert['path']
-        event['file_name'] = alert['name']
-        event['file_path'] = alert['tag']
+        if('change' in alert):  # Linux, normal pulsar
+            # The second half of the change will be "|IN_ISDIR" for directories
+            change = alert['change'].split("|")[0]
+            # Skip the IN_IGNORED events
+            if change == "IN_IGNORED":
+                continue
+            if len(alert['change'].split("|")) == 2:
+                object_type = 'directory'
+            else:
+                object_type = 'file'
 
-        if alert['stats']:  # Gather more data if the change wasn't a delete
-            stats = alert['stats']
-            event['object_id'] = stats['inode']
-            event['file_acl'] = stats['mode']
-            event['file_create_time'] = stats['ctime']
-            event['file_modify_time'] = stats['mtime']
-            event['file_size'] = stats['size'] / 1024.0  # Convert bytes to kilobytes
-            event['user'] = stats['user']
-            event['group'] = stats['group']
-            if object_type == 'file':
-                event['file_hash'] = alert['checksum']
-                event['file_hash_type'] = alert['checksum_type']
+            actions = defaultdict(lambda: 'unknown')
+            actions['IN_ACCESS'] = 'read'
+            actions['IN_ATTRIB'] = 'acl_modified'
+            actions['IN_CLOSE_NOWRITE'] = 'read'
+            actions['IN_CLOSE_WRITE'] = 'read'
+            actions['IN_CREATE'] = 'created'
+            actions['IN_DELETE'] = 'deleted'
+            actions['IN_DELETE_SELF'] = 'deleted'
+            actions['IN_MODIFY'] = 'modified'
+            actions['IN_MOVE_SELF'] = 'modified'
+            actions['IN_MOVED_FROM'] = 'modified'
+            actions['IN_MOVED_TO'] = 'modified'
+            actions['IN_OPEN'] = 'read'
+            actions['IN_MOVE'] = 'modified'
+            actions['IN_CLOSE'] = 'read'
+
+            event["action"] = actions[change]
+            event["change_type"] = 'filesystem'
+            event["object_category"] = object_type
+            event["object_path"] = alert['path']
+            event['file_name'] = alert['name']
+            event['file_path'] = alert['tag']
+
+            if alert['stats']:  # Gather more data if the change wasn't a delete
+                stats = alert['stats']
+                event['object_id'] = stats['inode']
+                event['file_acl'] = stats['mode']
+                event['file_create_time'] = stats['ctime']
+                event['file_modify_time'] = stats['mtime']
+                event['file_size'] = stats['size'] / 1024.0  # Convert bytes to kilobytes
+                event['user'] = stats['user']
+                event['group'] = stats['group']
+                if object_type == 'file':
+                    event['file_hash'] = alert['checksum']
+                    event['file_hash_type'] = alert['checksum_type']
+
+        else: # Windows, win_pulsar
+            change = alert['Accesses']
+            if alert['Hash'] == 'Item is a directory':
+                object_type = 'directory'
+            else:
+                object_type = 'file'
+
+            actions = defaultdict(lambda: 'unknown')
+            actions['Delete'] = 'deleted'
+            actions['Read Control'] = 'read'
+            actions['Write DAC'] = 'acl_modified'
+            actions['Write Owner'] = 'modified'
+            actions['Synchronize'] = 'modified'
+            actions['Access Sys Sec'] = 'read'
+            actions['Read Data'] = 'read'
+            actions['Write Data'] = 'modified'
+            actions['Append Data'] = 'modified'
+            actions['Read EA'] = 'read'
+            actions['Write EA'] = 'modified'
+            actions['Execute/Traverse'] = 'read'
+            actions['Read Attributes'] = 'read'
+            actions['Write Attributes'] = 'acl_modified'
+            actions['Query Key Value'] = 'read'
+            actions['Set Key Value'] = 'modified'
+            actions['Create Sub Key'] = 'created'
+            actions['Enumerate Sub-Keys'] = 'read'
+            actions['Notify About Changes to Keys'] = 'read'
+            actions['Create Link'] = 'created'
+            actions['Print'] = 'read'
+
+            event["action"] = actions[change]
+            event["change_type"] = 'filesystem'
+            event["object_category"] = object_type
+            event["object_path"] = alert['Object Name']
+            event['file_name'] = os.path.basename(alert['Object Name'])
+            event['file_path'] = os.path.dirname(alert['Object Name'])
+            # TODO: Should we be reporting 'EntryType' or 'TimeGenerated?
+            #   EntryType reports whether attempt to change was successful.
 
         event.update({"master": master})
         event.update({"dest_host": fqdn})
